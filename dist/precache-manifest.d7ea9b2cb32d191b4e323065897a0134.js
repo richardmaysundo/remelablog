d62cb4c52c89b16f62bed7217769cd9b90316a03
James Maysundo <richardmaysundo@gmail.com>
2022-01-30 21:17:47 -0500

    Creating dist folder to contain build files

---
+++ dist/precache-manifest.d7ea9b2cb32d191b4e323065897a0134.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d78b95c29138be025d555d21c7661b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "82032708f105cf90a991",
+    "url": "./static/css/main.723c8e3c.chunk.css"
+  },
+  {
+    "revision": "cd446c52e51c2a43d305",
+    "url": "./static/js/2.514ea27e.chunk.js"
+  },
+  {
+    "revision": "a7a59d960a5edb9089dc8f74ece8e150",
+    "url": "./static/js/2.514ea27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82032708f105cf90a991",
+    "url": "./static/js/main.3dd46b79.chunk.js"
+  },
+  {
+    "revision": "c2a917142daff62597d3",
+    "url": "./static/js/runtime-main.0a7ff063.js"
+  }
+]);
